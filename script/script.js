6f8e526235b2fa8b3a61118fdea5cd25bba5e8b6
Facundo JS <facubor26@gmail.com>
2023-02-13 19:02:03 -0300

    segunda entrega

--- script/script.js
+++ script/script.js
@@ -1,100 +1,90 @@
-// bienvenido
-function Ingreso (){
-    const year = 2023;
-    let nameUser = prompt ("Escriba su nombre");
-        while (nameUser == ""){
-            alert ("Campo vacio, escriba su nombre por favor.");
-            nameUser = prompt ("Escriba su nombre");
-        };
-    let surname = prompt ("Escriba su apellido");
-        while (surname == ""){ 
-            alert ("Campo vacio, escriba su apellido por favor.");
-            surname = prompt ("Escriba su apellido");
-        };
-    let dateOfBirth = parseInt(prompt ("Año de nacimiento"));
-        while (isNaN (dateOfBirth)){
-            alert ("Ingresaste un valor no numerico, por favor corrija");
-            dateOfBirth = parseInt(prompt ("Año de nacimiento"))
-        };
-    let age = year - dateOfBirth;
-    let userData = `Hola ${nameUser} ${surname}, segun mis calculos tenes ${age} años`;
-    alert (userData);
-    alert ("Hasta luego!")
-}
 
 
-// clima
-function clima (){
-    let clima = prompt ("Como esta el clima en tu ciudad (Caluroso, Lluvioso, Frio)");
-        while (clima == "") {
-            alert ("Campo vacio");
-            clima = prompt ("Como esta el clima en tu ciudad (Caluroso, Lluvioso, Frio)")
-        };
-        switch (clima) {
-            case "caluroso": {
-                alert ("Mantente Hidratado!")
-                break;
-            }
-            case "lluvioso":{
-                alert ("Si salis, lleva paraguas!")
-                break;
-                }
-            case "frio": {
-                alert ("mantente abrigado, tomando algo caliente!")
-                break;
-                }
-            default :{
-                alert ("Dato erroneo, Mire por la ventana, y vea su clima")
-                }
-            alert ('Good luck!')
-            }
-}
-
-
-
-// ingreso bar
-function ingresoBar () {
-    let edad = parseInt (prompt("ingrese su edad"))
-        while (isNaN (edad)){
-            alert ("Por favor ingrese numeros");
-            edad = parseInt (prompt("ingrese su edad"))
-            }
-        if (edad <21){
-            alert ("Usted no cumple con el rango de edad");
-            alert ("Eres muy pequenio");
-            }
-        else if (edad <=30){
-            alert ("Cumple con el rango de edad, puede ingresar");
-            alert ("Bienvenido");
-            }
-        else  {
-            alert ("usted sobrepasa el rango de edad")
-            alert ("Good luck!")
-        };
-}
-
-
-
-// casa de cambio
-function casaCambio() {
-    let values = parseInt (document.getElementById("valor").value) 
-    let result = 0;
-    let dolar = 380;
-    let euro = 404;
-        if (document.getElementById ("uno").checked){
-            result = values / dolar;
-            alert ("Su cambio es US$ "+result);
+function  start () {
+    const productos = [
+        {nombre:"Coca Cola" , precio:500 },
+        {nombre:"Sprite" , precio:480},
+        {nombre:"Fanta", precio:480},
+        {nombre:"Pasos de los toros" , precio:450},
+        {nombre:"Pepsi" , precio:450},
+        {nombre:"Agua mineral" , precio:300},
+    ];
+    console.log (productos);
+    let carroDeCompras = [];
+    
+    
+    alert ("Bienvenido")
+    let compra = prompt ("Desea realizar una compra... (si - no)");
+        while(compra != "si" && compra != "no"){
+            alert ("Dato erroneo, ingrese nuevamente... (si - no)")
+            compra = prompt ("Desea realizar una compra... (si - no)")
         }
-        else if (document.getElementById("dos").checked){
-            result = values / euro;
-            alert ("Su cambio es EUR€ "+result);
+        if(compra == "si"){
+            alert("Eliga su bebida...");
+            let todoLosProductos = productos.map((productos)=> productos.nombre + " " +"$"+ productos.precio);
+            alert(todoLosProductos.join(" - "));
+            } 
+            else if (compra == "no"){
+            alert("gracias por pasar, hasta pronto!")
+        }  
+    while (compra != "no"){
+        let producto = prompt ("Nombre del producto... (Coca cola, Sprite, Fanta, Pasos de los toros, Pepsi, Agua mineral ")
+        let precio = 0
+    
+        if(producto == "coca cola" || producto == "sprite" || producto == "fanta" || producto == "pasos de los toros" || producto == "pepsi" || producto == "agua mineral"){
+            switch (producto) {
+                case "Coca cola", "coca cola":
+                    precio = 500;
+                    break;
+                case "Sprite", "sprite":
+                    precio = 480;
+                    break;
+                case  "Fanta", "fanta":
+                    precio = 480;
+                    break;
+                case "Pasos de los toros", "pasos de los toros":
+                    precio = 450;
+                    break;
+                case "Pepsi", "pepsi":
+                    precio = 450;
+                    break;
+                case "Agua mineral", "agua mineral":
+                    precio = 300;
+                    break; 
+                default :
+                    alert ("Dato erróneo")
+            }
+            let unidades = parseInt(prompt("cuantas unidades queres llevar?"))
+            carroDeCompras.push({producto, unidades, precio})
+            console.log(carroDeCompras)
         }
         else {
-            alert ('Seleccione la moneda e ingrese un monto')
-        };
+            alert("Dato erróneo")
+        }
+        compra = prompt("Desea seguir comprando? si o no")
+        while(compra == "no"){
+            alert("gracias por la compra")
+            carroDeCompras.forEach((carroTotal) =>{
+                console.log(`Producto: ${carroTotal.producto}, Unidades: ${carroTotal.unidades}, Total a pagar por producto es: ${carroTotal.unidades * carroTotal.precio}`)
+            })
+            break;
+        }
+    }
+    const total = carroDeCompras.reduce((acumulador, el)=> acumulador + el.precio * el.unidades, 0)
+
+    console.log(`Su total a pagar es: ${total}`)
+    document.getElementById ('divTotal').innerHTML = `Su total a pagar es: $${total} pesos.`;
+    
+
 }
 
-    
+
+
+
+
+
+
+
 
 
 
